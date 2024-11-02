cc8e230ec3822313220e8fcb44a64ac05954e8f8
ideftbuild <a.akanSWE@gamil.com>
2024-11-02 15:20:09 +0100

    feat: Implement `get_hyper` method

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Module: 2-hypermedia_pagination"""
+import csv
+from typing import List, Dict
+
+
+def index_range(page: int, page_size: int):
+    """Returns a tuple containing the start and end index
+    corresponding to the range of indexes to return in a list
+    for those particular pagination parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page"""
+        try:
+            assert page >= 1 and page_size >= 1
+        except TypeError:
+            raise AssertionError
+        self.dataset()
+        start, end = index_range(page, page_size)
+        return self.__dataset[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Get paginated data along with hypermedia pagination details."""
+        total_pages = (len(self.dataset()) + page_size - 1) // page_size
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
