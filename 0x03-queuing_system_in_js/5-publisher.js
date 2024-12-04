415e5372cd1995ec03d520e12955c0f55a84fbb2
ideftbuild <a.akanSWE@gamil.com>
2024-12-04 14:20:51 +0100

    feat: Add scripts for subscribing to and publishing messages on a Redis channel

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.message);
+});
+
+const channel = 'holberton school channel';
+
+const publishMessage = (message, time) => {
+  console.log('About to send', message);
+  setTimeout(() => {
+    client.publish(channel, message);
+  }, time);
+};
+
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('KILL_SERVER', 300);
+publishMessage('Holberton Student #3 starts course ', 400);
