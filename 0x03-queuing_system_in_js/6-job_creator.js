815828780c37efc3dda63b8969c7838724e88675
ideftbuild <a.akanSWE@gamil.com>
2024-12-05 05:52:18 +0100

    feat: Add script that creates job

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const data = {
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
+};
+
+const job = queue.create('push_notification_code', data)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    } else {
+      console.log(`Failed to create job: ${err}`);
+    }
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
