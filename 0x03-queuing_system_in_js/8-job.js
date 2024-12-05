20cec72fe0f311719a6e57bb7878811953e8d9a0
ideftbuild <a.akanSWE@gamil.com>
2024-12-05 14:46:00 +0100

    feat: Add `createPushNotificationsJobs` function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((data) => {
+    const job = queue.create('push_notification_code_3', data).save((err) => {
+      if (err) {
+        console.error('Failed to create job:', err.message);
+      } else {
+        console.log('Notification job created:', job.id);
+      }
+    });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (percent) => {
+      console.log(`Notification job ${job.id} ${percent}% complete`);
+    });
+  });
+};
+
+export default createPushNotificationsJobs;
